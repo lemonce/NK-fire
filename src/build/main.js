691cd7658b9ef3a9e3b59f1dfc50478b1fec5926
PhoenSXar <phoensxar@gmail.com>
2017-12-18 09:03:46 +0800

    [ADDED] electron

---
+++ src/build/main.js
@@ -0,0 +1,44 @@
+const electron = require('electron');
+const http = require('http');
+const express = require('express');
+const expressApp = express();
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+expressApp.use(express.static('.'));
+
+http.createServer(expressApp).listen(8888);
+
+let mainWindow;
+
+function createWindow () {
+	mainWindow = new BrowserWindow({fullscreen: true});
+	mainWindow.setMenu(null);
+	mainWindow.loadURL(url.format({
+		pathname: 'localhost:8888',
+		protocol: 'http:',
+		slashes: true
+	}));
+
+	mainWindow.on('closed', function () {
+		mainWindow = null;
+	});
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', function () {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
